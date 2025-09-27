6c5f0a83d7474154ff17005bab3b49547bda7d5a
LïamA <augu.liam@gmail.com>
2025-09-27 15:50:11 +0200

    Add homepage layout and image, update styles
    Implemented a homepage layout in App.jsx with header, navigation, and main image. Added clairetest.webp image asset. Updated index.css to define a primary blue color variable. Updated package.json to include autoprefixer, postcss, and moved tailwindcss to devDependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import claire from "./assets/images/clairetest.webp"
+
 function App() {
   return (
     <div>
-      
+      <div id="homePage" className="flex justify-center items-center flex-col text-primary-blue">
+        <header className="w-full flex flex-col gap-9 py-14 items-center">
+          <h1 className="text-5xl py-3">Isabelle Augu</h1>
+          <nav className="w-7/10">
+            <ul className="flex justify-between">
+              <li>accueil</li>
+              <li>à propos</li>
+              <li>oeuvres</li>
+              <li>actualités</li>
+              <li>contact</li>
+            </ul>
+          </nav>
+        </header>
+        <main className="w-8/10">
+          <img src={claire} alt="Isabelle Augu" />
+        </main>
+      </div>
     </div>
   )
 }
