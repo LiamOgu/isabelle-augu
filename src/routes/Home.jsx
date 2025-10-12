8f5cd82b8b07d89a09dcd9039cc02e059d61658b
LïamA <augu.liam@gmail.com>
2025-10-12 19:57:02 +0200

    image d'accueil

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,8 +3,10 @@ import { Helmet } from "react-helmet";
 import Navbar from "../components/Navbar.jsx"
 import Header from "../components/Header.jsx"
 import Footer from "../components/Footer.jsx"
+import data from "../data.js";
 
 const Home = () => {
+  const imageAccueil = data.imagesAccueil[0];
   return (
     <div className="flex flex-col min-h-screen">
       <Helmet>
@@ -13,14 +15,14 @@ const Home = () => {
       </Helmet>
       <div id="homePage" className="flex justify-center items-center flex-col mb-12 flex-grow">
         <Header title="isabelle augu" textColor="text-purple-900" />
-        <main className="w-full md:w-8/10">
-          <div className="flex justify-center items-center w-full h-150">
-            Image en attente
-          </div>
+        <main className="w-full md:w-8/10 flex justify-center items-center">
+          <figure className="w-full h-full lg:w-3/5">
+            <img src={imageAccueil.src} className="w-full h-full" alt={imageAccueil.alt} />
+          </figure>
         </main>
-      </div>
+      </div >
       <Footer />
-    </div>
+    </div >
   )
 }
 
