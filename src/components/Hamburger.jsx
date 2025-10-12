5b778af3cddde97866322a5702d2682afeebab0e
LïamA <augu.liam@gmail.com>
2025-10-12 14:40:43 +0200

    fix : hamburger

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -5,17 +5,15 @@ import NavLinks from "./NavLinks.jsx"
 const HamburgerComponent = () => {
   const [isOpen, setIsOpen] = useState(false)
 
-  return (
-    <div className="lg:hidden absolute top-0 right-0 z-50">
-      {/* Bouton hamburger */}
-      <Hamburger toggled={isOpen} toggle={setIsOpen} size={24} />
 
-      {/* Menu mobile */}
+  return (
+    <div>
+      <div className="lg:hidden fixed top-2 right-2 z-50">
+        <Hamburger toggled={isOpen} toggle={setIsOpen} size={24} />
+      </div>
+
       {isOpen && (
-        <div className="absolute top-0 right-0 bg-white w-screen h-screen flex flex-col items-center justify-center gap-8">
-          <div className="lg:hidden absolute top-0 right-0 z-50">
-            <Hamburger toggled={isOpen} toggle={setIsOpen} size={24} />
-          </div>
+        <div className="fixed top-0 right-0 bg-white w-screen h-screen flex flex-col items-center justify-center gap-8 z-10">
           <NavLinks onClick={() => setIsOpen(false)} />
         </div>
       )}
