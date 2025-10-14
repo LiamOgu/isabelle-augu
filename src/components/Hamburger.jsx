3aa489a271faea13951e949a71fbb064df0d5097
LïamA <augu.liam@gmail.com>
2025-10-14 19:33:50 +0200

    add motion effect

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react"
 import Hamburger from "hamburger-react"
 import NavLinks from "./NavLinks.jsx"
+import { motion, AnimatePresence } from "motion/react"
 
 const HamburgerComponent = () => {
   const [isOpen, setIsOpen] = useState(false)
 
-
   return (
     <div>
+      {/* Bouton Hamburger */}
       <div className="lg:hidden fixed top-2 right-2 z-50 backdrop-blur-md rounded-md">
         <Hamburger toggled={isOpen} toggle={setIsOpen} size={24} />
       </div>
-
-      {isOpen && (
-        <div className="fixed top-0 right-0 w-screen h-screen bg-white dark:bg-gray-950 flex flex-col items-center justify-center gap-8 z-10">
-          <NavLinks onClick={() => setIsOpen(false)} />
-        </div>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            key="mobile-menu"
+            className="fixed top-0 right-0 w-screen h-screen bg-white dark:bg-gray-950 flex flex-col items-center justify-center gap-8 z-10"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2, ease: "easeInOut" }}
+          >
+            <NavLinks onClick={() => setIsOpen(false)} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
